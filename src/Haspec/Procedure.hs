cbdf13632e249702213c649fc3af0559f7613451
Maximilian Algehed <m.algehed@gmail.com>
2016-02-07 20:30:23 +0100

    More html stuff

--- src/Haspec/Procedure.hs
+++ src/Haspec/Procedure.hs
@@ -6,6 +6,7 @@ module Haspec.Procedure where
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html.Renderer.Pretty as R
 import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html4.FrameSet.Attributes as A'
 
 data Procedure dataType propertyType = Procedure
                                        {
@@ -16,6 +17,15 @@ data Procedure dataType propertyType = Procedure
                                        } 
                                        deriving (Ord, Eq, Show)
 
+mkArgumentsTable :: (Show d, Show p) => Procedure d p -> H.Html
+mkArgumentsTable procedure = H.table H.! A'.border (H.stringValue "1") $
+                                     H.tr $ H.th "Argument name and type" >> H.th "Must satisfy" >>
+                                     (foldl (>>) (return ()) $  
+                                     map (\(n, d, p) -> H.tr $ do
+                                                                H.td (H.toHtml (n ++ " : " ++ (show d)))
+                                                                H.td (H.toHtml (show p))
+                                         ) (arguments procedure))
+
 getDocumentation :: (Show d, Show p) => Procedure d p -> H.Html
 getDocumentation procedure = H.div H.!
                              A.id ((H.stringValue . name) procedure)
@@ -24,3 +34,4 @@ getDocumentation procedure = H.div H.!
                                 H.br
                                 H.b $ "Description:"
                                 H.p $ H.toHtml (docstring procedure)
+                                mkArgumentsTable procedure
