bbb7a2d24bdcf4ba6233f4bfeedc7a4ba4ab4761
Maximilian Algehed <m.algehed@gmail.com>
2016-02-22 22:56:42 +0100

    An experiment

--- src/Lang/Lang.hs
+++ src/Lang/Lang.hs
@@ -1,38 +1,39 @@
+{-# LANGUAGE GADTs, DataKinds, KindSignatures #-}
 import Data.Map.Strict (Map)
 
 data Protocol = Protocol
-{ pEndpoints :: [Endpoint]
-, pVersion   :: String
-}
+    { pEndpoints :: [Endpoint]
+    , pVersion   :: String
+    }
 
 data Endpoint = Endpoint
-{ eUrl           :: String
-, eTransmissions :: Map Method Transmission
-}
+    { eUrl           :: String
+    , eTransmissions :: Map Method Transmission
+    }
 
 data Method = GET | POST | OPTIONS | HEAD | PUT | DELETE
 
 -- | A Transmission is a bundle consisting of a request and a response.
 data Transmission = Transmission
-{ tRequest  :: Request
-, tResponse :: Response
-}
+    { tRequest  :: Request
+    , tResponse :: Response
+    }
 
 data Request = Request
-{ rqFields :: [Field]
-}
+    { rqFields :: [Field]
+    }
 
 data Response = Response
-{ reFields :: [Field]
-}
+    { reFields :: [Field]
+    }
 
 data Field = Field
-{ fName        :: String
-, fLabel       :: String
-, fDescription :: String
-, fType        :: FieldType
-, fValidators  :: [Validator]
-}
+    { fName        :: String
+    , fLabel       :: String
+    , fDescription :: String
+    , fType        :: FieldType
+    , fValidators  :: [Validator]
+    }
 
 data FieldType = FTInt
                | FTString
@@ -47,3 +48,26 @@ data Validator = Required Bool             -- ^ Is this field required? True by 
                | LowerBound Int            -- ^ Lower bound in length for strings and lists, and by value for numbers.
                | UpperBound Int            -- ^ Upper bound in length for strings and lists, and by value for numbers.
                | Chain Validator Validator -- ^ Combine two validators, both must pass for this validator to pass.
+
+-- | Datakind for token validity
+data Token where
+    ValidToken   :: Token
+    InvalidToken :: Token
+    deriving Show
+
+-- | Side effects that we can get from tokens
+data TokenSideEffect :: Token -> * where
+    Produce :: TokenSideEffect ValidToken
+    Require :: TokenSideEffect ValidToken
+    Consume :: TokenSideEffect InvalidToken
+
+login :: TokenSideEffect ValidToken
+login = Produce
+
+updateUsername :: String -> TokenSideEffect ValidToken -> TokenSideEffect ValidToken
+updateUsername = undefined
+
+logout :: TokenSideEffect ValidToken -> TokenSideEffect InvalidToken
+logout = const Consume
+
+transaction = logout $ updateUsername "Bob" login
