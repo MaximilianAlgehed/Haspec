9028b67caee6aada4165f23deb58e560b35c2eb0
Andreas Ekeroot <andeke@gmail.com>
2016-02-22 21:53:59 +0100

    A first, rough prototype of a language for specifiying REST APIs.

---
+++ src/Lang/Lang.hs
@@ -0,0 +1,49 @@
+import Data.Map.Strict (Map)
+
+data Protocol = Protocol
+{ pEndpoints :: [Endpoint]
+, pVersion   :: String
+}
+
+data Endpoint = Endpoint
+{ eUrl           :: String
+, eTransmissions :: Map Method Transmission
+}
+
+data Method = GET | POST | OPTIONS | HEAD | PUT | DELETE
+
+-- | A Transmission is a bundle consisting of a request and a response.
+data Transmission = Transmission
+{ tRequest  :: Request
+, tResponse :: Response
+}
+
+data Request = Request
+{ rqFields :: [Field]
+}
+
+data Response = Response
+{ reFields :: [Field]
+}
+
+data Field = Field
+{ fName        :: String
+, fLabel       :: String
+, fDescription :: String
+, fType        :: FieldType
+, fValidators  :: [Validator]
+}
+
+data FieldType = FTInt
+               | FTString
+               | FTBool
+               | FTDouble
+               | FTList FieldType
+               | FTMap FieldType FieldType -- I'm not sure about this one. We'll see if it's needed.
+
+-- | The validator type, I'm not at all sure about if this is a good idea.
+data Validator = Required Bool             -- ^ Is this field required? True by default.
+               | NullAllowed Bool          -- ^ Do we get away with not supplying this field? False by default.
+               | LowerBound Int            -- ^ Lower bound in length for strings and lists, and by value for numbers.
+               | UpperBound Int            -- ^ Upper bound in length for strings and lists, and by value for numbers.
+               | Chain Validator Validator -- ^ Combine two validators, both must pass for this validator to pass.
